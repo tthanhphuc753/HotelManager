b31d4e24d87c4310c09e8852e0cfa4d2db35cdc9
Daario-Naharis <112336332+Roizk@users.noreply.github.com>
2023-04-24 22:05:57 +0700

    Update Phong

---
+++ Phong.cs
@@ -0,0 +1,20 @@
+class Phong
+{
+for(int i: Phong)
+  {
+    if( select* from Phong where "tinh trang phong"+tinh_trang_phong+"gia phpng"+gia_phong)
+      {
+        MessageBox.show("OK vào đi em");
+      }
+     try
+      {
+      
+      }
+     catch(exception)
+      {
+        thrown
+          MessageBox.show("Hết phòng rồi em");
+      }
+  }
+
+}
